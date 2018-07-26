83a537ee608f30449cee0e4be9a53819170ae35a
monalisa behera <monalisa@monalisas-MacBook-Pro.local>
2018-07-26 19:23:57 +0530

    how to acess html atribute in java script

--- src/app.js
+++ src/app.js
@@ -1,1 +1,6 @@
-console.log('hello world!!')
+// console.log('hello world!!')
+// var h=document.getElementById('hello')
+// h.innerHTML="hi how r you?"
+
+
+console.log(document.getElementById('hello').getAttribute('reply'));
